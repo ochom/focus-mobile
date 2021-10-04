ed0713a801fa089c5b53846ccdef1b7e5504e5f9
ochom <ochomrichard752@gmail.com>
2021-10-04 13:01:35 +0300

    rename server.go

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/ochom/focus/domain"
@@ -9,20 +11,41 @@ import (
 )
 
 func main() {
+
 	arguments := os.Args
+
 	if len(arguments) != 3 {
 		fmt.Println(domain.USAGE)
 		return
 	}
+
 	action := arguments[1]
 	param := arguments[2]
+
 	switch action {
 	case "search":
 		helpers.Search(param)
 	case "describe":
 		helpers.Describe(param)
+	case "run":
+		runServer()
 	default:
 		fmt.Println(domain.USAGE)
 		return
+	}
+}
+
+func runServer() {
+	PORT := helpers.GetEnv("PORT")
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(rw, "Hello world")
+	})
+	http.HandleFunc("/register", helpers.Register())
+	http.HandleFunc("/add-rx", helpers.AddPrescriptions())
+
+	err := http.ListenAndServe(":"+PORT, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
